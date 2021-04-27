68c1c0d6cc40643ee4f7ff60a717a7e091a4d7ed
Bruno Purper Farias <brunoplusultra@outlook.com>
2021-04-26 22:06:41 -0300

    Create main.py

---
+++ Aula 26-04/main.py
@@ -0,0 +1,10 @@
+print("Sejam bem-vindos ao Instituto Infnet!")
+print("Informe o seu nome: ")
+
+#Vamos solicitar que o user informe seu nome
+#Para isso usaremos o input
+
+nome = input("Informe o seu nome: ")
+
+#O f é para concatenar o nome!
+print(f"Olá, {nome}, prazer em te conhecer!")
