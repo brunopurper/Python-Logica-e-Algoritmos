1ec245f2f476f56298168fdacd20ee3d34a3708e
Bruno Purper Farias <53791374+brunopurper@users.noreply.github.com>
2021-05-12 21:01:26 -0300

    Create main.py

---
+++ Maio/Aula 12-05/main.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 60:
+    print("Pode utilizar a vaga para idosos no estacionamento: ")
+else: 
+    print("Ainda não pode utilizar as vagas para idosos no estacionamento:")
